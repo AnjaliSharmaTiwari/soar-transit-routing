84c7a5ca9faa85837993074f6680a25201306dee
Anjali Sharma-Tiwari <47544996+AnjaliSharmaTiwari@users.noreply.github.com>
2023-12-11 15:14:55 -0800

    Update ondemandquery.py
    More input options for picking the simulation choice.

--- simulation/ondemandquery.py
+++ simulation/ondemandquery.py
@@ -13,6 +13,20 @@ os.system(f'{python_global_call} ./builders/build_TBTR_dict.py')
 os.system(f'{python_global_call} ./builders/build_transfer_patterns.py')
 os.system(f'{python_global_call} ./builders/build_CSA.py')
 
+def take_inputs() -> tuple:
+
+    algorithm = int(input(
+        "Press 0 to enter SOAR environment \nPress 1 to enter Walking environment\nPress 2 to enter Transfer Patterns environment\n: "))
+    variant = 0
+    print("***************")
+    if algorithm == 0:
+        variant = int(input(
+            "Press 0 for SOAR \nPress 1 for wSOAR \nPress 2 for One-To-Many wSOAR \n: "))
+    elif algorithm == 1:
+        variant = int(input("Press 0: SOAR \nPress 1: wSOAR \nPress 2: One-To-Many wSOAR \n: "))
+    print("***************")
+    return algorithm, variant
+
 
 def main() -> None:
     """
